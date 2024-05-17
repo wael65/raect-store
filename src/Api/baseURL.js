adbec438feb66cbbee49ad57c9b43fc7e067d1fb
waelebrahim65@gmail.com <waelebrahim65@gmail.com>
2024-05-17 15:23:47 +0300

    add functionality of search bar and fix baseURIr

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const baseUrl = axios.create({
-  // baseURL: "https://simple-backend-523x.onrender.com",
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://simple-backend-523x.onrender.com",
+  // baseURL: "http://127.0.0.1:8000",
 });
 
 export default baseUrl;
