8920d6a3a3ee5e644701bea97045c59b2be8fff7
wael65 <waelebrahim65@gmail.com>
2024-05-27 12:23:52 +0300

    add header in useInsertData

--- src/Api/useInsertData.js
+++ src/Api/useInsertData.js
@@ -1,7 +1,12 @@
 import baseUrl from "./baseURL";
 
 const useInsertData = async (url, parmas) => {
-  const res = await baseUrl.post(url, parmas);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  };
+  const res = await baseUrl.post(url, parmas, config);
   console.log(res);
   return res;
 };
